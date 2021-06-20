24aa1f866f1cb365d0d02fe531b57bf1342462e0
Gennadiy <9254497089@mail.ru>
2021-06-21 00:33:04 +0300

    Fiq Section question toggler animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,44 +1,42 @@
 import React from 'react';
 // Styles
 import styled from 'styled-components';
-import { About } from "../styles";
+import {About} from "../styles";
+
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
+
 
 const FaqSection = () => {
     return (
         <Fiq>
             <h2>Any Question <span>FIQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
+            <AnimateSharedLayout>
+            <Toggle title="How Do I Start?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, repudiandae!</p>
+                    </div>
+            </Toggle>
+            <Toggle title="Daily Schedule">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, repudiandae!</p>
+                </div>
+            </Toggle>
+            <Toggle title="Different Payment Methods">
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, repudiandae!</p>
                 </div>
-            </div>
-            <div className="faq-line"></div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
+            </Toggle>
+            <Toggle title="What Products do you offer?">
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, repudiandae!</p>
                 </div>
-            </div>
-            <div className="faq-line"></div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, repudiandae!</p>
-                </div>
-            </div>
-            <div className="faq-line"></div>
-            <div className="question">
-                <h4>What Products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, repudiandae!</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            </Toggle>
+            </AnimateSharedLayout>
         </Fiq>
     );
 };
